39e75da1c5e41bbac87c46858c349984c1e1cc3e
sachi345 <cmaheeshi88@gmail.com>
2022-08-18 21:30:35 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df1 = pd.read_excel(r'output_f1.xlsx')
+df2 = pd.read_excel(r'output_f2.xlsx')
+
+print(df1.shape)
+print(df2.shape)
+
+df_combined = pd.DataFrame(df1.merge(df2, on='cust_no', how='left'))
+print(df_combined)
+
+df_combined.to_excel('merged.xlsx')
